cdb7b5cf158e29eaf532be403732aad4e7adad61
Sylvain Pollet-Villard <sylvain.pollet.villard@gmail.com>
2024-03-24 23:42:54 +0100

    add back smeargle

--- app/types/Config.ts
+++ app/types/Config.ts
@@ -244,7 +244,8 @@ export const UniqueShop = new Array<PkmProposition>(
   Pkm.KANGASKHAN,
   Pkm.DRUDDIGON,
   Pkm.COSMOG,
-  Pkm.SCYTHER
+  Pkm.SCYTHER,
+  Pkm.SMEARGLE
 )
 
 export const LegendaryShop = new Array<PkmProposition>(
